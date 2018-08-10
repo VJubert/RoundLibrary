d2393ed875e53d79daf4d9472f919fbaea4045e6
Valentin Jubert <valentin.jubert@ideine.fr>
2018-08-10 10:00:04 +0200

    fix sample & delete resource.designer.cs

--- RoundSample/MainActivity.cs
+++ RoundSample/MainActivity.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using Android.App;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
-using Android.Support.Design.Widget;
 using Android.Support.V7.App;
-using Android.Views;
 
 namespace RoundSample
 {
 	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
 	public class MainActivity : AppCompatActivity
 	{
-
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            SetContentView(Resource.Layout.activity_main);
-        }
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			
+			SetContentView(Resource.Layout.activity_main);
+		}
 	}
 }
 
